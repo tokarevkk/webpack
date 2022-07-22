cf1373a2431dad39b5dee051d82585b1d4a65a0c
tokarevkk <tokarevkk@gmail.com>
2022-07-22 12:42:07 +0300

    massages page

---
+++ src/utils/router.js
@@ -0,0 +1,18 @@
+import { createPage } from "../user-page/user";
+import { createFriends } from "../frends-page/friends"
+import { createMessages } from "../messages-page/messages";
+
+
+export const switchRoutes = () => {
+   switch (window.location.pathname) {
+      case '/':
+         createPage()
+         break
+      case '/friends':
+         createFriends()
+         break
+         case '/messages':
+         createMessages()
+         break
+   }
+}
